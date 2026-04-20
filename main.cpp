2f4ef5db57c163edb63132d83e73f17fe90f400f
abyss of harmony <denysshpoltakov@gmail.com>
2026-04-20 17:30:14 +0200

    feat: rest healing function add

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@ enum MainMenu {
 enum AdventureAction {
     REST = 1,
     GO_HUNT = 2,
-    EXIT_TO_MAINMENU = 3
+    CHECK_STAT = 3,
+    EXIT_TO_MAINMENU = 4
 };
 
 struct Character {
@@ -17,12 +18,28 @@ struct Character {
     int level; // character's level
     int xp; // character's xp
     int hp; // character's health
+    int maxHP; // character's max HP
     int damage; // character's damage
     int gold; // character's gold
 };
 
 void showInfoAboutCharacter(Character &info) {
     cout << "Name: " << info.name << " Level: " << info.level << " XP: " << info.xp << " HP: " << info.hp << " Damage: " << info.damage << " Gold: " << info.gold << endl;
+}
+
+void restHealing(Character &player) {
+    int healCost = 5; // gold cost
+    int healAmount = 20; // how much hp you will recieve
+
+    if (player.hp >= player.maxHP) {
+        cout << "You do not have to heal" << endl;
+    } else if (player.gold < healCost) {
+        cout << "You do not have enough money for heal" << endl;
+    } else {
+        player.hp += healAmount;
+        player.gold -= healCost;
+        cout << "You were succesfully healed" << endl;
+    }
 }
 
 void playGame(Character &player) {
@@ -32,22 +49,29 @@ void playGame(Character &player) {
     while (isPlaying) {
         cout << "---> 1. Rest (Heal +20 HP) <---" << endl;
         cout << "---> 2. Go hunting <---" << endl;
-        cout << "---> 3. Exit (Back to main menu) <---" << endl;
+        cout << "---> 3. Check stats <---" << endl;
+        cout << "---> 4. Exit (Back to main menu) <---" << endl;
         cout << ">> ";
         cin >> choice;
 
-        if (choice == REST) {
+        if (choice == REST) { // you are healing your HP
             // healing function
             cout << "You are resting" << endl;
-        } else if (choice == EXIT_TO_MAINMENU) {
+            restHealing(player);
+
+        } else if (choice == CHECK_STAT) { // check your character's stat
+            showInfoAboutCharacter(player);
+
+        } else if (choice == EXIT_TO_MAINMENU) { // to exit in main menu
             isPlaying = false;
+
         }
     }
 }
 
 
 // Character's characteristics
-Character player = {"None", 1, 1, 100, 10, 0}; // name, level, xp, hp, damage, gold on start
+Character player = {"None", 1, 1, 40, 100, 10, 100}; // name, level, xp, hp, damage, gold on start
 Character enemy = {"Headcrab", 0, 10, 30, 5, 15}; // name, level, xp, hp, damage, gold
 
 
@@ -89,5 +113,4 @@ void showMainMenu() {
 
 int main() {
     showMainMenu();
-    showInfoAboutCharacter(player);
 }
