8847ec0f60d76e28077160af6a756e1c573f41b7
abyss of harmony <denysshpoltakov@gmail.com>
2026-04-21 22:15:44 +0200

    feat: add damage taking system, attack system and healing system

--- main.cpp
+++ main.cpp
@@ -24,9 +24,28 @@ public:
         cout << "Damage: ";
         cout << "--- " << damage << " ---" << endl;
     }
+    //==================== DAMAGE TAKING SYSTEM =============================
+    void takeDamage(int dmg) {
+        hp -= dmg;
+        if (hp < 0) {
+            hp = 0;
+        }
+    }
+    
+    //==================== ATTACK SYSTEM =============================
+    void attack(Character &attacker, Character &victim) {
+        // func...
+    }
+    
+    //==================== HEALING SYSTEM =============================
+    void heal(int amount) {
+        hp += amount;
+    }
 };
 
 int main() {
     Character mainHero("John", 100, 10); // name, health, damage
+    mainHero.showInfo();
+    mainHero.takeDamage(25);
     mainHero.showInfo();
 }
