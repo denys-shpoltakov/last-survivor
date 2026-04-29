5aaa6b6cd7d6366d0f9d706ce064800c1627e071
denys-shpoltakov <denysshpoltakov@gmail.com>
2026-04-29 14:57:21 +0200

    feat: add health in parent class and in constructor. Now every Entity will have health points

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ class BaseEntity {
 protected: // это что-то среднее между public или private, классы и его дети видят атрибуты, а main() - нет 
     string m_name; // имя сущности
     int m_damage; // урон сущности
-    BaseEntity(string name, int damage) : m_name(name), m_damage(damage) {} // Можно создавать только "детей". Это защищает от ошибок, когда ты случайно спавнишь в мире "объект-пустышку"
+    int m_health; // здоровье сущности
+    BaseEntity(string name, int damage, int health) : m_name(name), m_damage(damage), m_health(health) {} // Можно создавать только "детей". Это защищает от ошибок, когда ты случайно спавнишь в мире "объект-пустышку"
 public:
     virtual void talk() = 0; // визуальная функция, которая делает класс абстрактным
     virtual ~BaseEntity() {} // виртуальный диструктор. он необходим для корректного удаления производных объектов через указатель на базовый класс
@@ -14,7 +15,7 @@ public:
 
 class Player : public BaseEntity {
 public:
-    Player() : BaseEntity("Gordon", 20) {}
+    Player() : BaseEntity("Gordon", 20, 100) {}
 
     void talk() override {
         cout << "Hello World!" << endl;   
@@ -23,7 +24,7 @@ public:
 
 class Zombie : public BaseEntity {
 public:
-    Zombie() : BaseEntity("Headcrab", 10) {}
+    Zombie() : BaseEntity("Headcrab", 10, 20) {}
 
     void talk() override {
         cout << "Headcrab sound" << endl;
@@ -35,4 +36,5 @@ int main() {
     BaseEntity* Gordon = new Player();
     Gordon->talk();
     BaseEntity* Enemy = new Zombie();
+    Enemy->talk();
 }
