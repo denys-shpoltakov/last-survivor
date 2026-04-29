2d42b50763739be94293aa42be6def3d65b23eb9
denys-shpoltakov <denysshpoltakov@gmail.com>
2026-04-29 14:26:24 +0200

    feat: add class BaseEntity with protected attributes m_name and m_damage. Add constructor and you can't do empty objects

--- main.cpp
+++ main.cpp
@@ -1,55 +1,10 @@
 #include <iostream>
-#include <ctime>
-
 using namespace std;
-class Character {
-private:
-    string m_name;
-    int m_hp, m_damage;
 
-public:
-    //================ CONSTRUCTOR ==================
-    Character(string name, int hp, int damage) : m_name(name), m_hp(hp), m_damage(damage) {};
-
-    //===================== SHOWING CHARACTER'S CHARACTERISTICS ============================
-    void showInfo() {
-        cout << "Name: ";
-        cout << "--- " << m_name << " ---" << endl;
-        cout << "HP: ";
-        cout << "--- " << m_hp << " ---" << endl;
-        cout << "Damage: ";
-        cout << "--- " << m_damage << " ---" << endl;
-    }
-    //==================== DAMAGE TAKING SYSTEM =============================
-    void takeDamage(int damageAmount) {
-        m_hp -= damageAmount;
-        if (m_hp < 0) {
-            m_hp = 0;
-        }
-    }
-    
-    //==================== ATTACK SYSTEM =============================
-    void attack(Character &victim) {
-        // func...
-        cout << m_name << " attacks and dealing: " << m_damage << endl;
-        victim.takeDamage(m_damage);
-    }
-    
-    //==================== HEALING SYSTEM =============================
-    void heal(int amount) {
-        m_hp += amount;
-        if (m_hp > 100) {
-            m_hp = 100;
-        }
-    }
+class BaseEntity {
+    // по началу тут будет только два атрибута, имя сущности и урон сущности
+protected: // это что-то среднее между public или private, классы и его дети видят атрибуты, а main() - нет 
+    string m_name; // имя сущности
+    int m_damage; // урон сущности
+    BaseEntity(string name, int damage) : m_name(name), m_damage(damage) {} // Можно создавать только "детей". Это защищает от ошибок, когда ты случайно спавнишь в мире "объект-пустышку"
 };
-
-int main() {
-    Character mainHero("John", 100, 110); // name, health, damage
-    Character enemy("Zombie", 100, 10); // name, health, damage
-    mainHero.showInfo();
-    mainHero.attack(enemy);
-    mainHero.showInfo();
-    enemy.showInfo();
-
-}
