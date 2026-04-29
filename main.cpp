c30a0f9e7486663028f947d18fb5f982c36b6699
denys-shpoltakov <denysshpoltakov@gmail.com>
2026-04-29 15:18:41 +0200

    feat: add methods connected with attack and takeDamage

--- main.cpp
+++ main.cpp
@@ -9,13 +9,28 @@ protected: // это что-то среднее между public или private
     int m_health; // здоровье сущности
     BaseEntity(string name, int damage, int health) : m_name(name), m_damage(damage), m_health(health) {} // Можно создавать только "детей". Это защищает от ошибок, когда ты случайно спавнишь в мире "объект-пустышку"
 public:
+    virtual void takeDamage(int damage) {
+        m_health -= damage;
+        cout << m_name << " got " << damage << " HP: " << m_health << endl; 
+    }
+
+    string getName() {
+        return m_name;
+    }
+
+    virtual void attack(BaseEntity* target) = 0;
     virtual void talk() = 0; // визуальная функция, которая делает класс абстрактным
     virtual ~BaseEntity() {} // виртуальный диструктор. он необходим для корректного удаления производных объектов через указатель на базовый класс
 };
 
 class Player : public BaseEntity {
 public:
-    Player() : BaseEntity("Gordon", 20, 100) {}
+    Player() : BaseEntity("Gordon", 20, 100) {} // собираем игрока с именем: Gordon, с уроном: 20, с единицами здоровья: 100
+
+    void attack(BaseEntity* target) override {
+        cout << m_name << " attacks a: " << target->getName() << " and dealing: " << m_damage << " damage!" << endl;
+        target->takeDamage(m_damage);
+    }
 
     void talk() override {
         cout << "Hello World!" << endl;   
@@ -25,6 +40,11 @@ public:
 class Zombie : public BaseEntity {
 public:
     Zombie() : BaseEntity("Headcrab", 10, 20) {}
+
+    void attack(BaseEntity* target) override {
+        cout << m_name << " attacks a: " << target->getName() << " and dealing: " << m_damage << " damage!" << endl;
+        target->takeDamage(m_damage);
+    }
 
     void talk() override {
         cout << "Headcrab sound" << endl;
@@ -37,4 +57,5 @@ int main() {
     Gordon->talk();
     BaseEntity* Enemy = new Zombie();
     Enemy->talk();
+    Gordon->attack(Enemy);
 }
