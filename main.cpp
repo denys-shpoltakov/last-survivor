e023933be8f1662fb79bca93924b9f0e8f6d9507
denys-shpoltakov <denysshpoltakov@gmail.com>
2026-04-29 15:34:43 +0200

    add comment in 12 line

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ protected: // это что-то среднее между public или private
     int m_health; // здоровье сущности
     BaseEntity(string name, int damage, int health) : m_name(name), m_damage(damage), m_health(health) {} // Можно создавать только "детей". Это защищает от ошибок, когда ты случайно спавнишь в мире "объект-пустышку"
 public:
-    virtual void takeDamage(int damage) {
+    virtual void takeDamage(int damage) { // в параметр передаем урон, дальше мы туда передадим урон нашей сущности у которой будет вызываться эта функция
         m_health -= damage;
         if (m_health < 0) {
             m_health = 0;
